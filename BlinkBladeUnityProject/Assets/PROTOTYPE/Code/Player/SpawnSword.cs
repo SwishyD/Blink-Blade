b7024b89715055ff7f356ea58357ae5e6e6505bb
Connor <MXCapturing@gmail.com>
2019-07-25 15:12:31 +1200

    Sword Spawning started

---
+++ BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Player/SpawnSword.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSword : MonoBehaviour
+{
+
+    public GameObject sword;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(sword, Vector2.zero, Quaternion.identity);
+        }
+    }
+}
