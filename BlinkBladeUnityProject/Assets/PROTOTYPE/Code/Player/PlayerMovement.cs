2bd32dce652afb76b42302857ed0046754555374
Connor <MXCapturing@gmail.com>
2019-07-29 15:22:15 +1200

    Wall Jump
    + Double Jump
    + Wall Hanging
    + Jump Reset
    + Quick Fall

--- BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Player/PlayerMovement.cs
+++ BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Player/PlayerMovement.cs
@@ -3,38 +3,115 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
-
 {
+    public SwordSpawner spawner;
+
+    public bool canMove;
+
     public float speed;
-    public float JumpForce;
-    private Rigidbody2D _rb;
+    public Rigidbody2D _rb;
+
+    private float moveInput;
+    public float jumpForce;
+    public float doubleJumpForce;
+
+    public bool doubleJumpReady = false;
+    private bool isGrounded;
+    public Transform feetPos;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    public bool isHanging = false;
 
     private void Start()
     {
         _rb = gameObject.GetComponent<Rigidbody2D>();
+        canMove = true;
     }
+
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.A))
+        if (canMove)
         {
-            _rb.velocity = new Vector2(-speed, 0f);
+            HandleMove();
         }
-        if (Input.GetKeyUp(KeyCode.A))
+    }
+
+    private void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+
+        if(isGrounded == true && Input.GetKeyDown(KeyCode.W))
         {
-            _rb.velocity = new Vector2(0, 0);
+            Jump();
         }
-        if (Input.GetKey(KeyCode.D))
+        else if(isHanging == true && Input.GetKeyDown(KeyCode.W))
         {
-            _rb.velocity = new Vector2(speed, 0f);
+            Jump();
+            isHanging = false;
+            Destroy(spawner.CloneSword);
+            spawner.CloneSword = null;
+            spawner.swordSpawned = false;
         }
-        if (Input.GetKeyUp(KeyCode.D))
+        else if(doubleJumpReady == true && Input.GetKeyDown(KeyCode.W))
         {
-            _rb.velocity = new Vector2(0, 0);
+            DoubleJump();
         }
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space))
+
+        //Quick Fall
+        if (isGrounded == false && Input.GetKey(KeyCode.S))
         {
-            _rb.AddForce(new Vector2(0, JumpForce));
+            _rb.gravityScale = 10;
         }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            ResetGravity();
+        }
+
+        if(isHanging == true && Input.GetKey(KeyCode.S))
+        {
+            ResetGravity();
+            isHanging = false;
+            Destroy(spawner.CloneSword);
+            spawner.CloneSword = null;
+            spawner.swordSpawned = false;
+        }
+    }
+
+    public void HandleMove()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+        _rb.velocity = new Vector2(moveInput * speed, _rb.velocity.y);
+    }
+
+    public void Jump()
+    {
+        ResetGravity();
+        _rb.velocity = new Vector2(_rb.velocity.x, 0f);
+        _rb.velocity = Vector2.up * jumpForce;
+        doubleJumpReady = true;
+    }
+
+    public void DoubleJump()
+    {
+        ResetGravity();
+        _rb.velocity = new Vector2(_rb.velocity.x, 0f);
+        _rb.velocity = Vector2.up * doubleJumpForce;
+        doubleJumpReady = false;
+    }
+
+    public void ResetGravity()
+    {
+        canMove = true;
+        _rb.gravityScale = 5;
+    }
+
+    public void FreezePos()
+    {
+        _rb.velocity = Vector2.zero;
+        _rb.gravityScale = 0;
+        isHanging = true;
+        canMove = false;
     }
 }
--- BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Player/SwordSpawner.cs
+++ BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Player/SwordSpawner.cs
@@ -11,6 +11,8 @@ public class SwordSpawner : MonoBehaviour
 
     public GameObject CloneSword;
 
+    public GameObject player;
+
     public bool swordSpawned = false;
     // Update is called once per frame
     void FixedUpdate()
@@ -19,18 +21,33 @@ public class SwordSpawner : MonoBehaviour
         float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
 
-        if (Input.GetMouseButtonDown(0) && swordSpawned == false)
+        if (Input.GetMouseButtonDown(0) && swordSpawned == false && player.GetComponent<PlayerMovement>().isHanging == false)
         {
             CloneSword = Instantiate(sword, shotPoint.position, transform.rotation);
             swordSpawned = true;
         }
+        else if(Input.GetMouseButtonDown(0) && swordSpawned == true && player.GetComponent<PlayerMovement>().isHanging == true)
+        {
+            Destroy(CloneSword);
+            CloneSword = null;
+            player.GetComponent<PlayerMovement>().ResetGravity();
+            player.GetComponent<PlayerMovement>().isHanging = false;
+            CloneSword = Instantiate(sword, shotPoint.position, transform.rotation);
+            swordSpawned = true;
+        }
 
-        if(Input.GetMouseButton(1) && swordSpawned == true)
+        if(Input.GetMouseButton(1) && swordSpawned == true && CloneSword.GetComponent<SwordProjectile>().StuckinObject == false)
         {
             transform.parent.transform.position = CloneSword.transform.position;
+            //player.GetComponent<PlayerMovement>().doubleJumpReady = true;
             swordSpawned = false;
             Destroy(CloneSword);
             CloneSword = null;
+        }
+        else if (Input.GetMouseButton(1) && swordSpawned == true && CloneSword.GetComponent<SwordProjectile>().StuckinObject == true)
+        {
+            transform.parent.transform.position = CloneSword.transform.GetChild(0).transform.position;
+            player.GetComponent<PlayerMovement>().FreezePos();
         }
     }
 }
