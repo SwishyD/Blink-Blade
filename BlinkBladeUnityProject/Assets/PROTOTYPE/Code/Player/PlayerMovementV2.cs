aa473974c97b0a739c59adfff92686df4766d962
Cody <xiswishd@gmail.com>
2019-08-05 14:10:07 +1200

    Basic Jump/Movement redo
    Now split into two different scripts

---
+++ BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Player/PlayerMovementV2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementV2 : MonoBehaviour
+{
+    private float moveInput;
+
+    [Range(1, 10)]
+    public float moveSpeed;
+
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void HandleMove()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+    }
+}
