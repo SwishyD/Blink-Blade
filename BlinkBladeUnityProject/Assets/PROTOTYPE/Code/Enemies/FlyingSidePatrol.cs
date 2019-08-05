5a095c2d8656efb8eb84ba1879d3311d5aebc592
Connor <MXCapturing@gmail.com>
2019-08-05 15:27:11 +1200

    Checkpoints & FlyingPatrol

---
+++ BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Enemies/FlyingSidePatrol.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PatrolDir { Left, Right, Up, Down}
+
+public class FlyingSidePatrol : MonoBehaviour
+{
+    public PatrolDir direction;
+    public float patrolTime;
+    public float maxPatrolTime;
+    public float speed;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        patrolTime += Time.deltaTime;
+        if(patrolTime >= maxPatrolTime)
+        {
+            if(direction == PatrolDir.Left || direction == PatrolDir.Up)
+            {
+                direction++;
+                patrolTime = 0;
+            }
+            else if(direction == PatrolDir.Right || direction == PatrolDir.Down)
+            {
+                direction--;
+                patrolTime = 0;
+            }
+        }
+
+        switch (direction)
+        {
+            case PatrolDir.Left:
+                transform.position -= new Vector3(speed, 0,0);
+                break;
+
+            case PatrolDir.Right:
+                transform.position += new Vector3(speed, 0, 0);
+                break;
+
+            case PatrolDir.Up:
+                transform.position += new Vector3(0, speed, 0);
+                break;
+
+            case PatrolDir.Down:
+                transform.position -= new Vector3(0, speed, 0);
+                break;
+        }
+    }
+}
---
+++ BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Systems/CheckPoint.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    public bool triggered;
+
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Player")
+        {
+            GetComponent<SpriteRenderer>().color = Color.red;
+            triggered = true;
+            PlayerMovement.instance.spawnPoint = this.transform.position;
+        }
+    }
+}
