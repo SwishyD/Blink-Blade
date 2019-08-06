0ba20786833b4b885d98fcbdf894e30c8aa9e58f
Connor <MXCapturing@gmail.com>
2019-08-06 15:26:59 +1200

    Platform Patrol Enemies + Shooting Enemies

---
+++ BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Enemies/EnemyShoot.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShoot : MonoBehaviour
+{
+    public bool active;
+    public GameObject bullet;
+    public int numberOfShots;
+
+    public GameObject bulletAimer;
+    public GameObject bulletSpawn;
+    public float offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Shoot");
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            active = true;
+        }
+        if (gameObject.GetComponent<FlyingEnemy>().isHit)
+        {
+            active = false;
+        }
+
+        Vector3 difference = GameObject.FindGameObjectWithTag("Player").transform.position - bulletAimer.transform.position;
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        bulletAimer.transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
+    }
+
+    IEnumerator Shoot()
+    {
+        while (enabled)
+        {
+            Debug.Log("Shoot");
+            if (active)
+            {
+                for (int i = 0; i < numberOfShots; i++)
+                {
+                    Instantiate(bullet, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+                    yield return new WaitForSeconds(0.1f);
+                }
+            }
+            yield return new WaitForSeconds(2f);
+        }
+    }
+}
--- BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Player/SwordSpawner.cs
+++ BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Player/SwordSpawner.cs
@@ -35,7 +35,7 @@ public class SwordSpawner : MonoBehaviour
     void FixedUpdate()
     {
         Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        //Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
         float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
 
