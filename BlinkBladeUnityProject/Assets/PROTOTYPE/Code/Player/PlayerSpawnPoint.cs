03911b2725e862776bdb1a3ee96cf1bb087cf293
Jaesda Kim <signupdump1@gmail.com>
2019-08-23 13:32:46 +1200

    Added Player Death/Respawn PFX

--- BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Player/PlayerSpawnPoint.cs
+++ BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Player/PlayerSpawnPoint.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerSpawnPoint : MonoBehaviour
 {
     public Vector2 spawnPoint;
+    public ParticleSystem deathPFX;
 
     // Start is called before the first frame update
     void Start()
@@ -23,11 +24,13 @@ public class PlayerSpawnPoint : MonoBehaviour
 
     public void Respawn()
     {
+        Instantiate(deathPFX, gameObject.transform.position, Quaternion.identity);
         gameObject.transform.position = spawnPoint;
         Destroy(SwordSpawner.instance.cloneSword);
         SwordSpawner.instance.cloneSword = null;
         SwordSpawner.instance.swordSpawned = false;
         PlayerJumpV2.instance.ResetGravity();
         PlayerJumpV2.instance.PlayerNormal();
+        Instantiate(deathPFX, transform);
     }
 }
