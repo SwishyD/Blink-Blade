c6f2b6d57326a1e4a1c485ff64c0ec41d69ffa25
Connor <MXCapturing@gmail.com>
2019-08-23 12:58:57 +1200

    Armour Respawn

--- BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Enemies/ArmourEnemy.cs
+++ BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Enemies/ArmourEnemy.cs
@@ -7,11 +7,32 @@ public class ArmourEnemy : MonoBehaviour, IEnemyDeath
     public GameObject soul;
     public GameObject soulSpawner;
 
+    public bool isHit;
+    public float respawnTimer;
+
     public void OnHit()
     {
+        GetComponent<Collider2D>().enabled = false;
+        isHit = true;
         GetComponent<SpriteRenderer>().color = Color.blue;
         var Soul = Instantiate(soul, soulSpawner.transform.position, Quaternion.identity);
         Physics2D.IgnoreCollision(Soul.GetComponent<Collider2D>(), GetComponent<Collider2D>());
-        this.gameObject.layer = 10;
+        StartCoroutine("Respawn");
+    }
+
+    IEnumerator Respawn()
+    {
+        yield return new WaitForSeconds(respawnTimer);
+        GetComponent<Collider2D>().enabled = true;
+        GetComponent<SpriteRenderer>().color = Color.white;
+        isHit = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player" && !isHit)
+        {
+            col.GetComponent<PlayerSpawnPoint>().Respawn();
+        }
     }
 }
