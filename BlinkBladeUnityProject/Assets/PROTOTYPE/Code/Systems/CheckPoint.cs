ce8083fe13a9df4ccc7cb7341e4ebaefcebeca74
Cody <xiswishd@gmail.com>
2019-08-26 15:28:43 +1200

    Timer Included

--- BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Systems/CheckPoint.cs
+++ BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Systems/CheckPoint.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class CheckPoint : MonoBehaviour
 {
     public bool triggered;
+    public bool isEnd;
+
+    public GameObject timer;
 
 
     private void OnTriggerEnter2D(Collider2D col)
     {
         if(col.tag == "Player" && triggered == false)
         {
+            if (isEnd)
+            {
+                timer.GetComponent<Timer>().TimerToggle();
+            }
             GetComponent<SpriteRenderer>().color = Color.red;
             triggered = true;
             GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerSpawnPoint>().spawnPoint = this.transform.position;
