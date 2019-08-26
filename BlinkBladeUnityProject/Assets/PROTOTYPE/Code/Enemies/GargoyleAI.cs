c00a76be5d14a412c84feeab57d175f50d2802f8
Unknown <MXCapturing@gmail.com>
2019-08-26 22:57:34 +1200

    Gargoyle and Banana Shockwaves

--- BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Enemies/GargoyleAI.cs
+++ BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Enemies/GargoyleAI.cs
@@ -4,12 +4,61 @@ using UnityEngine;
 
 public class GargoyleAI : MonoBehaviour
 {
+    public bool inRange;
+    [Tooltip("Number of tiles away from the Gargoyle to detect the player")]
+    public float detectionRange;
+    [Tooltip("Number in Seconds between each shockwave being released")]
+    public float timeBetweenShots;
+    public float chargeUpTime;
+
+
+    public Transform leftSide;
+    public Transform rightSide;
+
+    public GameObject shockWave;
+
+    private void Start()
+    {
+        StartCoroutine("ShockWave");
+    }
+
+    private void FixedUpdate()
+    {
+        if(Vector2.Distance(transform.position, GameObject.Find("PlayerV2").transform.position) <= detectionRange)
+        {
+            inRange = true;
+        }
+        else
+        {
+            inRange = false;
+        }
+        Debug.Log(inRange);
+    }
 
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.tag == "Player")
         {
             col.GetComponent<PlayerSpawnPoint>().Respawn();
+        }
+    }
+
+    IEnumerator ShockWave()
+    {
+        while (true)
+        {
+            if (inRange)
+            {
+                yield return new WaitForSeconds(chargeUpTime);
+                GetComponent<SpriteRenderer>().color = Color.yellow;
+                yield return new WaitForSeconds(0.2f);
+                Instantiate(shockWave, leftSide.position, Quaternion.identity);
+                var rightShock = Instantiate(shockWave, rightSide.position, Quaternion.identity);
+                rightShock.GetComponent<SpriteRenderer>().flipX = true;
+                rightShock.GetComponent<ShockwaveMovement>().isRight = true;
+                GetComponent<SpriteRenderer>().color = Color.white;
+            }
+            yield return new WaitForSeconds(timeBetweenShots);
         }
     }
 }
---
+++ BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Enemies/ShockwaveMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShockwaveMovement : MonoBehaviour
+{
+    public float timeBeforeMoving;
+    public int numberOfTiles;
+    public bool isRight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("ShockMove");
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            col.GetComponent<PlayerSpawnPoint>().Respawn();
+        }
+    }
+
+    IEnumerator ShockMove()
+    {
+        for (int i = 0; i < numberOfTiles; i++)
+        {
+            if (isRight)
+            {
+                transform.position += Vector3.right;
+            }
+            else if (!isRight)
+            {
+                transform.position += Vector3.left;
+            }
+            yield return new WaitForSeconds(timeBeforeMoving);
+        }
+        Destroy(gameObject);
+    }
+}
