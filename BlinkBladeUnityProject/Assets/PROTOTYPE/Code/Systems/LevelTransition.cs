d670b4331581988ebc520e95fa41016a2865dc2d
Connor <MXCapturing@gmail.com>
2019-09-06 13:23:46 +1200

    SceneTransition Scripts

---
+++ BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Systems/LevelTransition.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTransition : MonoBehaviour
+{
+    public string moveToLevel;
+
+    bool inDoor;
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.name == "PlayerV2")
+        {
+            if(gameObject.name.Contains("Door"))
+            {
+                inDoor = true;
+            }
+            else if (!gameObject.name.Contains("Door"))
+            {
+                SceneManagers.instance.MoveToScene(moveToLevel);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.name == "PlayerV2")
+        {
+            if (gameObject.name.Contains("Door"))
+            {
+                inDoor = false;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if(inDoor && Input.GetKeyDown(KeyCode.E))
+        {
+            SceneManagers.instance.MoveToScene(moveToLevel);
+        }
+    }
+}
