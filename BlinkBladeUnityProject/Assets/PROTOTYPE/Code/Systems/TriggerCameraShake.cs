b4f80671e9f0ea2fbd78da76f2ab28f252170d33
Jaesda Kim <signupdump1@gmail.com>
2019-09-10 15:29:14 +1200

    Added Camera Shake

---
+++ BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Systems/TriggerCameraShake.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerCameraShake : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            FindObjectOfType<CameraShaker>().StartCamShakeCoroutine(0.5f,0.3f,0.8f);
+        }
+    }
+}
