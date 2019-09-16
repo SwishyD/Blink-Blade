ffa94d09fb13ac5f9d4ddc87d5e16eb54dceff62
Jaesda Kim <signupdump1@gmail.com>
2019-09-16 14:46:14 +1200

    Optimised AdjustTooltipPos.cs

--- BlinkBladeUnityProject/Assets/PROTOTYPE/Code/UI/GUI/AdjustTooltipPos.cs
+++ BlinkBladeUnityProject/Assets/PROTOTYPE/Code/UI/GUI/AdjustTooltipPos.cs
@@ -3,11 +3,12 @@
 public class AdjustTooltipPos : MonoBehaviour
 {
     [SerializeField] Transform tooltipTransform;
+    [SerializeField] float upDist;
     
     // Update is called once per frame
     void Update()
     {
         Vector3 tooltipPos = Camera.main.WorldToScreenPoint(transform.position);
-        tooltipTransform.transform.position = tooltipPos;
+        tooltipTransform.transform.position = new Vector3(tooltipPos.x, tooltipPos.y + upDist, tooltipPos.z);
     }
 }
