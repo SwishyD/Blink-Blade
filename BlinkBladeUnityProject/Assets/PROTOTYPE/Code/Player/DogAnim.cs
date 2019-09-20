6c0b4ac2e375a155d55cef1783b9b2d7307205e9
Jaesda Kim <signupdump1@gmail.com>
2019-09-20 13:11:59 +1200

    Enemy Outlines Fade, Beginning of Dog

---
+++ BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Player/DogAnim.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogAnim : MonoBehaviour
+{
+    Animator anim;
+    float timeUntilTrig;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeUntilTrig > 0)
+        {
+            timeUntilTrig -= Time.deltaTime;
+        }
+        if (timeUntilTrig <= 0)
+        {
+            anim.SetFloat("IdleAnimIndex", Random.Range(1, 2));
+        }
+    }
+
+
+}
