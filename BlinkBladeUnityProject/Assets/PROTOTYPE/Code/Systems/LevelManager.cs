7ec0672e1bf4b2e4d79aade1292af16767bc343e
Connor <MXCapturing@gmail.com>
2019-10-07 14:18:57 +1300

    Save Load System started

---
+++ BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Systems/LevelManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public float time;
+    public int deaths;
+    public string grade;
+    public bool levelComplete;
+
+    public Vector2 playerPos;
+
+    public static LevelManager instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else if(instance != this)
+        {
+            Destroy(this);
+        }
+    }
+
+    private void Update()
+    {
+        playerPos = GameObject.Find("PlayerV2").transform.position;
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            SaveSystem.SavePlayer(this);
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            LevelData data = SaveSystem.LoadData();
+
+            time = data.levelTimes;
+            deaths = data.levelDeaths;
+            grade = data.levelGrades;
+            levelComplete = data.levels;
+
+            Vector2 position;
+            position.x = data.playerPosition[0];
+            position.y = data.playerPosition[1];
+            GameObject.Find("PlayerV2").transform.position = position;
+        }
+    }
+}
---
+++ BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Systems/SaveSystem.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class SaveSystem
+{
+    public static void SavePlayer(LevelManager manager)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/levels.blink";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        LevelData data = new LevelData(manager);
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static LevelData LoadData()
+    {
+        string path = Application.persistentDataPath + "/levels.blink";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            LevelData data = formatter.Deserialize(stream) as LevelData;
+            stream.Close();
+
+            return data;
+        }
+        else
+        {
+            Debug.LogError("Save file not found in" + path);
+            return null;
+        }
+    }
+}
