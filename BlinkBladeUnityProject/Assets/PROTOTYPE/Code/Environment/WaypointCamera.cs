b700f15ee61f2e64beec93cb3daafd9180b7a58a
Connor <MXCapturing@gmail.com>
2019-10-16 15:25:39 +1300

    Boss Level testing started

---
+++ BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Environment/WaypointCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointCamera : MonoBehaviour
+{
+    public Transform[] wayPoints;
+    public int moveToward;
+
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (wayPoints.Length > 1)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, wayPoints[moveToward].position, speed * Time.deltaTime);
+        }
+    }
+}
