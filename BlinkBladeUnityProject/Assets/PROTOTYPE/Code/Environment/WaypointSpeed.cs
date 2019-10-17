420d0eec4e6a7b9753346f0f90176ecc36fcbbdd
Connor <MXCapturing@gmail.com>
2019-10-17 15:17:03 +1300

    Boss Camera Movement continued

---
+++ BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Environment/WaypointSpeed.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointSpeed : MonoBehaviour
+{
+    [Tooltip("Speed the Camera moves to next Waypoint")]
+    public float speed;
+    [Tooltip("(seconds) Time the camera waits here until moving to next waypoint")]
+    public float waitTime;
+}
