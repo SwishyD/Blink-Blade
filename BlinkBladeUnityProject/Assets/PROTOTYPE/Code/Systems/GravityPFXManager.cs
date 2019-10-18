d41db7a5a64b3eda982788a4b0a3239a4197b753
Jaesda Kim <signupdump1@gmail.com>
2019-10-18 13:28:26 +1300

    Fixes to Hub, Spruces to GravFlipPFX

---
+++ BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Systems/GravityPFXManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityPFXManager : MonoBehaviour
+{
+    [SerializeField] ParticleSystem gravPFXCeil;
+    [SerializeField] ParticleSystem gravPFXFloor;
+
+    [SerializeField] bool gravFlipped;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChangeGravPFX(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            gravFlipped = !gravFlipped;
+            ChangeGravPFX(gravFlipped);
+        }
+    }
+
+    void ChangeGravPFX(bool gravFlipped)
+    {
+        var ceilEmission = gravPFXCeil.emission;
+        var floorEmission = gravPFXFloor.emission;
+        if (gravFlipped)
+        {
+            ceilEmission.rateOverTime = 0;
+            floorEmission.rateOverTime = 50;
+        }
+        if (!gravFlipped)
+        {
+            ceilEmission.rateOverTime = 50;
+            floorEmission.rateOverTime = 0;
+        }
+    }
+}
