db24003a3f284ecf037e53f8c26ad6e038f48096
Connor <MXCapturing@gmail.com>
2019-11-06 14:48:03 +1300

    Secret Room access complete

--- BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Environment/DogBowlHub.cs
+++ BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Environment/DogBowlHub.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class DogBowlHub : MonoBehaviour
 {
-    bool playerEnter;
     public Sprite[] bowlFullness;
     public int[] requiredTreats;
     [Space]
@@ -12,45 +11,20 @@ public class DogBowlHub : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            playerEnter = true;
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            playerEnter = false;
-        }
-    }
-
-    private void Update()
-    {
-        if (playerEnter)
-        {
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                noTreatsCollected = 0;
-                for (int i = 0; i < LevelManager.instance.dogTreatCollected.Length; i++)
-                {
-                    if (LevelManager.instance.dogTreatCollected[i])
-                    {
-                        noTreatsCollected++;
-                    }
-                }
-                SetDogBowl();
-            }
-        }
+        Invoke("SetDogBowl", 1f);
     }
 
     void SetDogBowl()
     {
+        noTreatsCollected = 0;
+        for (int i = 0; i < LevelManager.instance.dogTreatCollected.Length; i++)
+        {
+            if (LevelManager.instance.dogTreatCollected[i])
+            {
+                noTreatsCollected++;
+            }
+        }
+
         if (noTreatsCollected == requiredTreats[0])
         {
             GetComponent<SpriteRenderer>().sprite = bowlFullness[1];
