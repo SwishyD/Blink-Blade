fbc659964c8f6ec84ab4ed216d723a73e50bc7d5
Connor <MXCapturing@gmail.com>
2019-11-06 13:45:01 +1300

    stoopid dog bowl done

--- BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Environment/DogBowlHub.cs
+++ BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Environment/DogBowlHub.cs
@@ -4,16 +4,72 @@ using UnityEngine;
 
 public class DogBowlHub : MonoBehaviour
 {
+    bool playerEnter;
+    public Sprite[] bowlFullness;
+    public int[] requiredTreats;
+    [Space]
     public int noTreatsCollected;
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < LevelManager.instance.dogTreatCollected.Length; i++)
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
         {
-            if (LevelManager.instance.dogTreatCollected[i])
+            playerEnter = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerEnter = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (playerEnter)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
             {
-                noTreatsCollected++;
+                noTreatsCollected = 0;
+                for (int i = 0; i < LevelManager.instance.dogTreatCollected.Length; i++)
+                {
+                    if (LevelManager.instance.dogTreatCollected[i])
+                    {
+                        noTreatsCollected++;
+                    }
+                }
+                SetDogBowl();
             }
-        }   
+        }
+    }
+
+    void SetDogBowl()
+    {
+        if (noTreatsCollected == requiredTreats[0])
+        {
+            GetComponent<SpriteRenderer>().sprite = bowlFullness[1];
+        }
+        else if (requiredTreats[0] < noTreatsCollected && noTreatsCollected <= requiredTreats[1])
+        {
+            GetComponent<SpriteRenderer>().sprite = bowlFullness[2];
+        }
+        else if (requiredTreats[1] < noTreatsCollected && noTreatsCollected <= requiredTreats[2])
+        {
+            GetComponent<SpriteRenderer>().sprite = bowlFullness[3];
+        }
+        else if (noTreatsCollected == requiredTreats[3])
+        {
+            GetComponent<SpriteRenderer>().sprite = bowlFullness[4];
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().sprite = bowlFullness[0];
+        }
     }
 }
