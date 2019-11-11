65592fafb0ac61836af678b0be6b6f26bd9ffdc5
Connor <MXCapturing@gmail.com>
2019-11-11 14:05:11 +1300

    Volume Save started

---
+++ BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Systems/VolumeData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class VolumeData
+{
+    public float volume;
+
+    public VolumeData (VolumeSettings volumeManager)
+    {
+        volume = volumeManager.volumeSlider.value;
+    }
+}
---
+++ BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Systems/VolumeSaveSystem.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class VolumeSaveSystem
+{
+    public static void SavePlayer(VolumeSettings volumeManager)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/levelsvolume.blink";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        VolumeData data = new VolumeData(volumeManager);
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static VolumeData LoadData()
+    {
+        string path = Application.persistentDataPath + "/levelsvolume.blink";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            VolumeData data = formatter.Deserialize(stream) as VolumeData;
+            stream.Close();
+
+            return data;
+        }
+        else
+        {
+            Debug.Log("Save file not found in" + path);
+            return null;
+        }
+    }
+
+    public static void DeleteSave()
+    {
+        string path = Application.persistentDataPath + "/levelsvolume.blink";
+        File.Delete(path);
+    }
+
+    public static bool DataExists()
+    {
+        string path = Application.persistentDataPath + "/levelsvolume.blink";
+        if (File.Exists(path))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
