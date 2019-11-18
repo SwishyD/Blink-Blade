0f973dff481d59dfcbcfca1ee0136206e25c3183
Jaesda Kim <signupdump1@gmail.com>
2019-11-18 15:24:56 +1300

    Finished Intro Cutscene Anims

---
+++ BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Systems/CutsceneManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneManager : MonoBehaviour
+{
+    public GameObject cuff1;
+    public GameObject cuff2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CamShake()
+    {
+        FindObjectOfType<CameraShaker>().StartCamShakeCoroutine(1f, 0.1f, .7f);
+    }
+
+    public void SpawnCuffs()
+    {
+        cuff1.SetActive(true);
+        cuff2.SetActive(true);
+        cuff1.GetComponent<Rigidbody2D>().angularVelocity = 1080;
+        cuff1.GetComponent<Rigidbody2D>().AddForce(new Vector2(-200, 300));
+        cuff2.GetComponent<Rigidbody2D>().angularVelocity = -827;
+        cuff2.GetComponent<Rigidbody2D>().AddForce(new Vector2(200, 300));
+    }
+
+    public void EndScene()
+    {
+        SceneManagers.instance.MoveToScene("TUTORIAL");
+    }
+}
