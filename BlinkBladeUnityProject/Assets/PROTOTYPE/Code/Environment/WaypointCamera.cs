7d5323e034ca210595d9cb1b6583408f6b00bcd0
Jaesda Kim <signupdump1@gmail.com>
2019-11-19 14:52:37 +1300

    Fixed Layering Issue in Boss Fight, Added Frame

--- BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Environment/WaypointCamera.cs
+++ BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Environment/WaypointCamera.cs
@@ -14,7 +14,7 @@ public class WaypointCamera : MonoBehaviour
     private bool atEnd;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (!atEnd)
         {
