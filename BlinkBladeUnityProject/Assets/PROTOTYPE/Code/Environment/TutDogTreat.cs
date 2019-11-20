dba050b5c452a7849ca9c6194c16efd6d4e61a7d
Jaesda Kim <signupdump1@gmail.com>
2019-11-20 15:22:39 +1300

    Tutorial Dog Treat Box now has Effects

---
+++ BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Environment/TutDogTreat.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutDogTreat : MonoBehaviour
+{
+    //public LevelResultTransfer transfer;
+
+    public int addDebugTime;
+
+    private float requiredTime;
+
+    [SerializeField] ParticleSystem grabPFX;
+
+    private void Start()
+    {
+        //requiredTime = endResults.requiredTime[0] + addDebugTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            LevelManager.instance.dogTreatCollected[12] = true;
+            Instantiate(grabPFX, transform.position, Quaternion.identity);
+            Destroy(this.gameObject.transform.parent.gameObject);
+        }
+    }
+}
+
