8c3f585abef47baf641911efda030bbec4259d90
Connor <MXCapturing@gmail.com>
2019-11-21 13:54:36 +1300

    Tutorial Dog treat saving

--- BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Systems/LoadGame.cs
+++ BlinkBladeUnityProject/Assets/PROTOTYPE/Code/Systems/LoadGame.cs
@@ -16,6 +16,9 @@ public class LoadGame : MonoBehaviour
             LevelManager.instance.time[i] = data.levelTimes[i];
             LevelManager.instance.deaths[i] = data.levelDeaths[i];
             LevelManager.instance.grade[i] = data.levelGrades[i];
+        }
+        for (int i = 0; i < LevelManager.instance.dogTreatCollected.Length; i++)
+        {
             LevelManager.instance.dogTreatCollected[i] = data.dogTreat[i];
         }
         LevelManager.instance.playerPos.x = data.playerPosition[0];
