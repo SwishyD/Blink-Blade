93a951c11fc95853504482fd6779183fba4fba0b
Jaesda Kim <signupdump1@gmail.com>
2019-11-29 14:16:34 +1300

    Added TutorialTooltip Script

---
+++ BlinkBladeUnityProject/Assets/PROTOTYPE/Code/UI/GUI/TutorialTooltip.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialTooltip : MonoBehaviour
+{
+    string tagName = "Player";
+
+    [SerializeField] bool tooltipActiveState;
+
+    [SerializeField] GameObject tooltip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //tooltipAnim = tooltip.GetComponentInParent<Animator>();
+        SwitchTooltipActivationState(tooltipActiveState);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == tagName)
+        {
+            SwitchTooltipActivationState(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == tagName)
+        {
+            SwitchTooltipActivationState(false);
+        }
+    }
+
+    void SwitchTooltipActivationState(bool newState)
+    {
+        tooltipActiveState = newState;
+        tooltip.SetActive(newState);
+    }
+}
